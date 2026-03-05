f3223e1994ee02543432e0bec81bdd559e74cb01
cxzhong <chenxin.zhong@outlook.com>
2026-03-05 16:38:39 +0800

    Fix build script to copy liblrs.dll to bin directory on Windows

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,6 +15,8 @@ export CC="${CC} ${CPPFLAGS} ${CFLAGS} ${LDFLAGS}"
 make INCLUDEDIR=${PREFIX}/include LIBDIR=${PREFIX}/lib prefix=$PREFIX all-shared install -j${CPU_COUNT} $OPTIONS
 if [[ "$target_platform" == "win-64" ]]; then
   cp ${PREFIX}/bin/lrs.exe ${PREFIX}/bin/redund.exe
+  # DLLs must be on PATH; copy liblrs.dll to bin/ where executables live
+  cp ${PREFIX}/lib/liblrs.dll ${PREFIX}/bin/
 else
   ln -sf ${PREFIX}/bin/lrs ${PREFIX}/bin/redund
 fi
