5da3e6aecf4af6e3205e100b05190265d7beee76
cxzhong <chenxin.zhong@outlook.com>
2026-03-05 15:50:29 +0800

    Add Windows support in build scripts and update meta.yaml for cross-platform compatibility

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-if [[ "$target_platform" == "osx-64" ]]; then
-  sed -i.bak "s/-Wl,-soname=/-install_name /g" makefile
+if [[ "$target_platform" == "win-64" ]]; then
+  export PREFIX=${PREFIX}/Library
+  # MinGW doesn't use soname; replace with --out-implib to produce import library
+  sed -i "s/-Wl,-soname=/-Wl,--out-implib=/g" makefile
+  OPTIONS="SONAME=liblrs.dll.a SHLIB=liblrs.dll SHLINK=liblrs.dll.a"
+elif [[ "$target_platform" == osx-* ]]; then
+  sed -i "s/-Wl,-soname=/-Wl,-install_name,/g" makefile
   OPTIONS="SONAME=liblrs.0.dylib SHLIB=liblrs.0.0.0.dylib SHLINK=liblrs.dylib"
 fi
-export CC="${CC} ${CPPFLAGS} ${CFLAGS} ${LDFLAGS}"
+export CC="${CC} ${CPPFLAGS} ${CFLAGS}"
+export LDFLAGS="${LDFLAGS}"
 make INCLUDEDIR=${PREFIX}/include LIBDIR=${PREFIX}/lib prefix=$PREFIX all-shared install -j${CPU_COUNT} $OPTIONS
-ln -sf ${PREFIX}/bin/lrs ${PREFIX}/bin/redund
+if [[ "$target_platform" == "win-64" ]]; then
+  cp ${PREFIX}/bin/lrs.exe ${PREFIX}/bin/redund.exe
+else
+  ln -sf ${PREFIX}/bin/lrs ${PREFIX}/bin/redund
+fi
