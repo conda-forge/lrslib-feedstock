558a35db3a9db2edf98c48a451a47b168960500f
Chenxin Zhong <chenxin.zhong@outlook.com>
2026-03-10 01:30:37 +0800

    Update recipe/build.sh
    Co-authored-by: Isuru Fernando <isuruf@gmail.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -16,7 +16,7 @@ make INCLUDEDIR=${PREFIX}/include LIBDIR=${PREFIX}/lib prefix=$PREFIX all-shared
 if [[ "$target_platform" == "win-64" ]]; then
   cp ${PREFIX}/bin/lrs.exe ${PREFIX}/bin/redund.exe
   # DLLs must be on PATH; copy liblrs.dll to bin/ where executables live
-  cp ${PREFIX}/lib/liblrs.dll ${PREFIX}/bin/
+  mv ${PREFIX}/lib/liblrs.dll ${PREFIX}/bin/
 else
   ln -sf ${PREFIX}/bin/lrs ${PREFIX}/bin/redund
 fi
